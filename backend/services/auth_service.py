551ebc98b127f4d54b17b381184e7b448a0d6b58
UQBAH09 <khan.uqbah@gmail.com>
2026-04-24 22:51:00 +0500

    Add auth_service.py and main.py

---
+++ backend/services/auth_service.py
@@ -0,0 +1,28 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+import os
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+load_dotenv()
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+def hash_password(password):
+    return pwd_context.hash(password)    
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_token(user_id, role):
+    expiry = datetime.now(timezone.utc) + timedelta(hours=24)
+    payload = {"user_id":user_id, "role":role, "exp":expiry}
+
+    return jwt.encode(payload, JWT_SECRET, algorithm=["HS256"])
+
+def decode_token(token):
+    try:
+        return jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+
+    except Exception:
+        raise ValueError("Invalid or expired token")
